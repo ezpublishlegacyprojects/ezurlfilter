3447a74dfc688c44850b450e23844a5440cd9d8d
Kristof Coomans <kristof.coomans@telenet.be>
2008-07-29 08:33:06 +0000

    - Adding stable 1.0 branch   (Copied from trunk rev. 3)
    git-svn-id: http://svn.projects.ez.no/ezurlfilter@6 e8a8fb7a-e151-4cf5-8794-064754bac911

---
+++ stable/1.0/extension/ezurlfilter/ezinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+class ezurlfilterInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => '<a href="http://projects.ez.no/ezurlfilter">eZURL Filter</a>',
+            'Version' => "1.x",
+            'Copyright' => "Copyright (C) 2006-2008 Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+
+?>
